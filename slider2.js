1e96fe4096d190c2e3d842f744965e2353291310
Per Wikman <Per.Wikman@pensionsmyndigheten.se>
2016-12-20 10:29:38 +0100

    sliders moving in v4

--- slider2.js
+++ slider2.js
@@ -1,6 +1,6 @@
 function slider(d1,d2)
 {
-    var margin = {top: 40, left: 40, right: 30, bottom: 5},
+    var margin = {top: 40, left: 20, right: 30, bottom: 5},
         width  = 600 - margin.left - margin.right,
         height = 100  - margin.top  - margin.bottom,
         brush  = d3.brushX(),
@@ -17,17 +17,66 @@ function slider(d1,d2)
     function chart(el)
     {
 //http://www.rajvansia.com/scatterplotbrush-d3-v4.html
-        brush.extent([[0, 0], [width, height]])
-             .on("brush", brushed);
+        //brush.extent([[0, 0], [width, height]])
+        //     .on("brush", brushed);
 
         var svg = el.attr("width",  width  + margin.left + margin.right)
             .attr("height", height + margin.top  + margin.bottom)
             .append("g").attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
+        /*
         svg.append("g")
            .attr("class","x axis")
            .attr("transform", "translate(0,"+height/2+")")
            .call(d3.axisBottom(x).tickSize(0).tickPadding(12));
+*/
+
+
+           var slider = svg.append("g")
+       .attr("class", "slider")
+       .attr("transform", "translate(" + margin.left + "," + height / 2 + ")");
+
+   slider.append("line")
+       .attr("class", "track")
+       .attr("stroke", "dodgerblue")
+       .attr("x1", x.range()[0])
+       .attr("x2", x.range()[1])
+     .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+       .attr("class", "track-inset")
+     .select(function() { return this.parentNode.appendChild(this.cloneNode(true)); })
+       .attr("class", "track-overlay")
+       .call(d3.drag()
+           .on("start.interrupt", function() { slider.interrupt(); })
+          .on("start drag", function() { hue(x.invert(d3.event.x)); }));
+
+   slider.insert("g", ".track-overlay")
+       .attr("class", "ticks")
+       .attr("transform", "translate(0," + 18 + ")")
+     .selectAll("text")
+     .data(x.ticks(10))
+     .enter().append("text")
+       .attr("x", x)
+       .attr("text-anchor", "middle")
+       .text(function(d) { return d + "°"; });
+
+   var handle = slider.insert("circle", ".track-overlay")
+       .attr("class", "handle")
+       .attr("r", 9);
+
+   slider.transition() // Gratuitous intro!
+       .duration(750)
+       .tween("hue", function() {
+         var i = d3.interpolate(0, 70);
+         return function(t) { hue(i(t)); };
+       });
+
+
+   function hue(h) {
+     handle.attr("cx", x(h));
+     svg.style("background-color", d3.hsl(h, 0.8, 0.8));
+   }
+
+   /*
 
         slider = svg.append("g")
             .attr("class","slider")
@@ -56,6 +105,7 @@ function slider(d1,d2)
             value = brush.selection()[0];
             handle.attr("cx",x(value));
         }
+        */
     }
 
     chart.margin   = function(_) { if (!arguments.length) return margin;  margin = _; return chart; };
